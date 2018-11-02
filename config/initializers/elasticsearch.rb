b001875d18490c258873a3d5dd2eadbc72812ab6
pablocaro <caropablojavier@gmail.com>
2018-11-02 16:40:14 -0300

    agregada url bonsai

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,7 @@
 Kaminari::Hooks.init if defined?(Kaminari::Hooks)
 Elasticsearch::Model::Response::Response.__send__ :include, Elasticsearch::Model::Response::Pagination::Kaminari
 
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] || "http://localhost:9200/"
+Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] || "https://j9h7bp9f7h:dwdqwi0b0b@rowan-5612310.us-east-1.bonsaisearch.net"
 unless Producto.__elasticsearch__.index_exists?
   Producto.__elasticsearch__.create_index! force: true
   Producto.import
