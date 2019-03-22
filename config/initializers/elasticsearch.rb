14d64c8584cc7411b78b20f510b7922911878bd0
Pablo Caro <caropablojavier@gmail.com>
2019-03-21 21:45:04 -0300

    test elasticsearch reset

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,8 +1,10 @@
 Kaminari::Hooks.init if defined?(Kaminari::Hooks)
 Elasticsearch::Model::Response::Response.__send__ :include, Elasticsearch::Model::Response::Pagination::Kaminari
 
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] || "https://j9h7bp9f7h:dwdqwi0b0b@rowan-5612310.us-east-1.bonsaisearch.net"
+
+Elasticsearch::Model.client = Elasticsearch::Client.new url: "https://j9h7bp9f7h:dwdqwi0b0b@rowan-5612310.us-east-1.bonsaisearch.net"
 unless Producto.__elasticsearch__.index_exists?
+  Producto.__elasticsearch__.client.indices.delete index: Producto.index_name rescue nil
   Producto.__elasticsearch__.create_index! force: true
   Producto.import
 end
